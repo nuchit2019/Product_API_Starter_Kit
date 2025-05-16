e7e80ebaeac3f7e3dce6e93b245778782712a116
NuchitA <52587437+nuchit2019@users.noreply.github.com>
2025-05-16 20:28:17 +0700

    Update ProductService.cs

--- ProductAPI-Starter/ProductAPI/Application/Services/ProductService.cs
+++ ProductAPI-Starter/ProductAPI/Application/Services/ProductService.cs
@@ -16,39 +16,39 @@ namespace ProductAPI.Application.Services
 
         public async Task<IEnumerable<ProductResponseDTO>> GetAllAsync()
         {
-            // TODO 01 [Service]: Implement ...
+            // TODO 01 [Service.GetAllAsync]: Implement ...
             return null;
         }
 
         public async Task<ProductResponseDTO?> GetByIdAsync(int id)
         {
-            // TODO 02 [Service]: Implement ...
+            // TODO 02 [Service.GetByIdAsync]: Implement ...
             return null;
         }
 
         public async Task<ProductResponseDTO> CreateAsync(ProductCreateDTO productDto)
         {
-            // TODO 03 [Service]: Implement ...
+            // TODO 03 [Service.CreateAsync]: Implement ...
             return null;
 
         }
 
         public async Task<bool> UpdateAsync(ProductUpdateDTO updateProductDto)
         {
-            // TODO 04 [Service]: Implement ...
+            // TODO 04 [Service.UpdateAsync]: Implement ...
             return false ;
 
         }
 
         public async Task<bool> DeleteAsync(int id)
         {
-            // TODO 05 [Service]: Implement ...
+            // TODO 05 [Service.DeleteAsync]: Implement ...
             return false ;
         }
 
         private static ProductResponseDTO MapToDTO(Product product)
         {
-            // TODO 06 [Service]: Implement ...
+            // TODO 06 [Service.MapToDTO]: Implement ...
             return null;
         }
     }
